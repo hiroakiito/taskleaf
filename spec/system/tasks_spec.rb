3ddc6451dea30201e1f4831f5efbea989bb6985d
hiroakiito <pigaru.gooood.mar19@gmail.com>
2018-12-27 13:43:00 +0900

    5-10

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -5,22 +5,27 @@ describe 'タスク管理機能', type: :system do
     before do
       user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
       FactoryBot.create(:task, name: '最初のタスク', user: user_a)
-      #ユーザーAを作成しておく
-      #作成しゃがユーザーAであるタスクを作成しておく
+      visit login_path
+      fill_in 'メールアドレス', with: "a@example.com"
+      fill_in 'パスワード', with: "password"
+      click_button 'ログインする'
     end
 
     context 'ユーザーAがログインしている時' do
+      it 'ユーザーAが作成したタスクが表示される' do
+        #作成済みのタスクの名称が画面上に表示されていることを確認
+        expect(page).to have_content '最初のタスク'
+      end
+    end
+
+    context 'ユーザーBがログインしている時' do
       before do
-        #ユーザーAでログインする
-        visit login_path
-        fill_in 'メールアドレス', with: "a@example.com"
-                fill_in 'パスワード', with: "password"
-        click_button 'ログインする'
+        FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')
       end
 
       it 'ユーザーAが作成したタスクが表示される' do
         #作成済みのタスクの名称が画面上に表示されていることを確認
-        expect(page).to have_content '最初のタスク'
+        expect(page).to have_no_content '最初のタスク'
       end
     end
   end
