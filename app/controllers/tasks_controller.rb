adea57b5698e66793b878526c1e342b4d5ecec60
hiroakiito <pigaru.gooood.mar19@gmail.com>
2018-12-31 21:14:29 +0900

    7-7-6

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
 
   def index
     @q = current_user.tasks.ransack(params[:q])
-    @tasks = @q.result(distinct: true)
+    @tasks = @q.result(distinct: true).page(params[:page])
 
     respond_to do |format|
       format.html
@@ -51,6 +51,11 @@ class TasksController < ApplicationController
   def confirm_new
     @task = current_user.tasks.new(task_params)
     render :new unless @task.valid?
+  end
+
+  def import
+    current_user.tasks.import(params[:file])
+    redirect_to tasks_url, notice: "タスクを追加しました"
   end
 
   private
